a7419fac274bd76ce107abdb6ac2c86ff54abaa5
Olivia Kim <kimhaejeong2525@gmail.com>
2022-05-31 07:00:05 -0400

    header added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from './components/Header';
 import Nav from './components/Nav';
 function App() {
+  const [pages] = useState([
+    {
+      name: "about me"
+    },
+    { name: "portfolio" },
+    { name: "contact" },
+    {
+      name: "resume"
+    }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+  
   return (
     <div>
-      <header>
-      <Nav pages={pages} setCurrentpage={setCurrentpage} currentpage={currentpage}></Nav>
+      <Header>
+      <Nav pages={pages} 
+      setCurrentpage ={setCurrentpage} 
+      currentpage={currentpage}>
+      </Nav>
       <main>
 
       </main>
       <footer>
 
       </footer>
-      </header>
+      </Header>
      
     </div>
   );
