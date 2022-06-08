7d26cf0174d0ef5da40f27efc591920eca2b84b4
Olivia Kim <kimhaejeong2525@gmail.com>
2022-06-07 21:32:06 -0400

    another project added

--- src/components/Main/Projectcards.js
+++ src/components/Main/Projectcards.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import image from "../../assets/boxes/musicbox.png";
-
+import image2 from "../../assets/boxes/screenshot.png";
 function Projectcards() {
   return (
     <div className='cards'>
         <img src={image} style={{ width: "30%" }} alt="Musicbox"/>
+        <img src={image2} style={{width:"30%"}} alt="Smartstore"/>
     </div>
+    
   )
 }
 
